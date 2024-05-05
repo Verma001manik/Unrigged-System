643464edadd0656b9de300c73876de6c7f4b5e78
Manik Verma <verma001manik@gmail.com>
2024-05-05 17:58:56 +0530

    done done

--- Prob.py
+++ Prob.py
@@ -4,19 +4,25 @@ def rig_prob(w: int, n: int ):
     
      
     for _ in range(n):
-        count = 0 
+        count1 = 0
+        count0 = 0 
         arr = []
         for i in range(10):
             r  = random.randint(0,1)
-            if r ==1 and count ==w:
+            if r==0 and count0 <10-w:
+                arr.append(r)
+                count0 +=1 
+            elif r==1 and count1 < w :
+                arr.append(r)
+                count1 +=1 
+            elif r==1 and count1==w and count0 <10-w:
                 arr.append(0)
-            elif r==1 and count <w:
-                arr.append(r)
-                count += 1 
-            
-            else:
-                arr.append(r)
+                count0+= 1 
+            elif r==0 and count0==10-w and count1< w:
+                arr.append(1)
+                count1+=1 
+
         print(arr)
 
 
-r = rig_prob(3, 20)
+r = rig_prob(7,  100)
